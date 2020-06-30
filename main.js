cfd7e35cd8a42b30517fb6b24667eb1095cb35d2
Victor <victsales@gmail.com>
2020-06-30 11:24:30 -0300

    Changing code to make a countdown before the game starts on a modal popup, changing the content of h1 with promises! A simple Exercise of Javascript and CSS on this HTML game!

--- main.js
+++ main.js
@@ -34,3 +34,67 @@ function initGame(){
     //It will add a new ball to the dom tree every 0.25 seconds
     setInterval(addBall,250)
 }
+
+// const body = document.querySelector("body");
+// body.onload = initGame;
+function changeTo3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("3");
+        },1000);
+    });
+}
+
+function changeTo2(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("2");
+        },1000);
+    });
+}
+
+function changeTo1(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("1");
+        },1000);
+    });
+}
+
+function changeToStart(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("GO!!");
+        },1000);
+    });
+}
+
+function HideModal(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve();
+        },1000);
+    });
+}
+
+changeTo3()
+    .then((resposta)=>{
+        document.querySelector("#modal .content h1").innerHTML = resposta;
+        changeTo2()
+            .then((resposta=>{
+                document.querySelector("#modal .content h1").innerHTML = resposta;
+                changeTo1()
+                    .then((resposta)=>{
+                        document.querySelector("#modal .content h1").innerHTML = resposta;
+                        changeToStart()
+                            .then((resposta)=>{
+                                document.querySelector("#modal .content h1").innerHTML = resposta;
+                                HideModal()
+                                    .then(()=>{
+                                        document.querySelector("#modal").classList.add("hide");
+                                        initGame();
+                                    });
+                            });
+                    });
+            }));
+    });
