95a37b77a8286b9c537672241c0e2003e4feac17
Victor <victsales@gmail.com>
2020-06-30 15:46:16 -0300

    Creatuing structure to save socre data to the local Storage

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+class Score {
+    constructor(name,date,score) {
+      this.name = name;
+      this.date = date;
+      this.pontuation = score;
+    }
+  }
+
 //variable to store the number of bubbles we've popped! 
 let score = 0;
 let interval;
 let iterations;
+let allScores = [];
+
+function loadScores(){
+    // Get the older scores stored on the browser at the local storage!
+    return new Promise((resolve, reject)=>{
+        allScores = JSON.parse(localStorage.getItem('scores_list')) || []; //in case there's nothing at the local storage, load an empty list
+        resolve(allScores);
+    });
+}
+
+function saveScoreToStorage(){
+    //save the new scores to the local storage!
+    localStorage.setItem('scores_list',JSON.stringify(allScores));
+}
 
 //Function to be called when the element is clicked
 function removeBall(element){
@@ -35,7 +57,8 @@ function addBall(){
     newBall.setAttribute("style","left:"+left+"px; top:"+top+"px; height:"+diameter+"px; width:"+diameter+"px;")
     document.querySelector("body").appendChild(newBall);
     //In case the addBall function was called 240 times, stop adding balls
-    if(iterations >= 240){
+    // if(iterations >= 240){
+    if(iterations >= 40){
         clearInterval(interval);
         stopGameAndDisplayScore();
     }
@@ -90,14 +113,53 @@ function HideModal(){
     });
 }
 
+function reloadAndDisplayResults(){
+    //function to create a new score, store it on the local storage and reload the html to show the list of scores
+    // let ThisScore
+    if(document.querySelector("#input-submit").value == ""){
+        alert("Please, enter the name to save the score properly!!")
+    }
+    else{
+        let newScore = new Score(document.querySelector("#input-submit").value, Date.now(),score);
+        allScores.push(newScore);
+        saveScoreToStorage();
+        score = 0;
+    }
+}
+
+function DisplayAllScores(){
+
+}
+
 function stopGameAndDisplayScore(){
+    //Get the body elemennt
     let body = document.querySelector("body");
+    //Clear the html inside the tag body to clear the bubbles that we didn't pop yet
     body.innerHTML = "";
+    //Creates a new DOM Element (h1)
     let newH1 = document.createElement("h1");
     newH1.innerHTML = "Your Score: " + score;
+    //Creates a new div.form
+    let newInput = document.createElement("input");
+    newInput.setAttribute("placeholder","Type your Name in");
+    newInput.setAttribute("type","text");
+    newInput.setAttribute("id","input-submit")
+    //create a button to submit the new score to the list
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute("id","btn-submit");
+    newBtn.onclick = reloadAndDisplayResults;
+    //newBtn.innerHTML = "Submit New Score";
+    //Create a new div.form
+    let newDivForm = document.createElement("div");
+    newDivForm.classList.add("form");
+    newDivForm.appendChild(newInput);
+    newDivForm.appendChild(newBtn);
+    //Creates a new div with a class content2 and h1 and div.form as child nodes 
     let newContent = document.createElement("div");
     newContent.classList.add("content2");
     newContent.appendChild(newH1);
+    newContent.appendChild(newDivForm);
+    //Creates a modal with this two main divs inside it and appends this newModal into the body tag on DOM nodes tree
     let newModal = document.createElement("div");
     newModal.setAttribute("id","modal");
     newModal.appendChild(newContent);
@@ -129,4 +191,8 @@ function callCountdown(){
     });
 }
 
-callCountdown();
+loadScores()
+    .then(()=>{
+        callCountdown();
+    })
+// callCountdown();
